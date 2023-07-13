a207e89ea8ae5a167834d1cb5d1ddcfa6695458e
Sourabhsd87 <102124662+Sourabhsd87@users.noreply.github.com>
2023-07-13 17:51:23 +0530

    Add files via upload

---
+++ E_Commerce/E_commerse/E_commerse/Dashboard.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerse
+{
+    public class Dashboard : IOrderDetails, ICustomerDetails
+    {
+        #region Data members
+        private int FinancialYear;
+        private string Department;
+        private bool DisposedValue;
+        #endregion
+
+        #region getters n setters
+        public int financialYear
+        {
+            get { return this.FinancialYear; }
+            set { this.FinancialYear = value; }
+        }
+        public string department
+        {
+            get { return this.Department; }
+            set { this.Department = value; }
+        }
+        public bool disposedValue
+        {
+            get { return this.DisposedValue; }
+            set { this.DisposedValue = value; }
+        }
+        #endregion
+
+        #region ctors
+        public Dashboard() { }
+        public Dashboard(int FinancialYear,string Department,bool DisposedValue)
+        {
+            this.FinancialYear=FinancialYear;
+            this.Department=Department;
+            this.DisposedValue=DisposedValue;
+        }
+        #endregion
+
+        #region Implemented Methods
+        void IOrderDetails.ShowDetails()
+        {
+            Console.WriteLine("Showing Order Details.......");
+        }
+
+        void ICustomerDetails.ShowDetails()
+        {
+            Console.WriteLine("Showing Customer Details.........");
+        }
+        #endregion
+
+        #region dtor
+        ~Dashboard() { }
+        #endregion
+
+        public override string ToString()
+        {
+            return this.FinancialYear+" | "+this.department+" | "+this.DisposedValue;
+        }
+    }
+}
---
+++ E_Commerce/E_commerse/E_commerse/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using E_commerse;
+
+Console.WriteLine("Hello, World!");
+
+Dashboard dashboard = new Dashboard();
+//Dashboard dashboard1 = new Dashboard(1,"HR",true);
+
+dashboard.financialYear = 2022;
+dashboard.disposedValue = true;
+dashboard.department = "Sales";
+
+IOrderDetails details = new Dashboard(1, "HR", true);
+ICustomerDetails details1 = new Dashboard(2, "Security", false);
+
+details.ShowDetails();
+details1.ShowDetails();
+

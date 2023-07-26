9308091998b38931c805c99a0e0773d64d67eaf8
harshal0910 <harshalnagtilak2809@gmail.com>
2023-07-26 16:06:27 +0530

    Movie based scenario with implemented CRUD operations and Sorting of the movies

---
+++ MovieMania/MovieMania/Controllers/MoviesController.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MovieMania.Data;
+using MovieMania.Models;
+
+namespace MovieMania.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Movies
+        public async Task<IActionResult> Index()
+        {
+              return _context.Moviess != null ? 
+                          View(await _context.Moviess.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Moviess'  is null.");
+        }
+
+        // GET: Movies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Moviess == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Moviess
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // GET: Movies/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,movieName,producer,ticket,releaseDate")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(movie);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Moviess == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Moviess.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,movieName,producer,ticket,releaseDate")] Movie movie)
+        {
+            if (id != movie.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(movie);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MovieExists(movie.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Moviess == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Moviess
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Moviess == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Moviess'  is null.");
+            }
+            var movie = await _context.Moviess.FindAsync(id);
+            if (movie != null)
+            {
+                _context.Moviess.Remove(movie);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MovieExists(int id)
+        {
+          return (_context.Moviess?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+        public IActionResult Sort()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Sorting(string producer)
+        {
+            List<Movie> sortedList = new List<Movie>();
+            List<Movie> MovieList = await _context.Moviess.ToListAsync();
+
+            foreach (Movie M in MovieList) { 
+            
+                if(M.producer == producer)
+                {
+                    sortedList.Add(M);
+                }
+            }
+
+            return View(sortedList);
+        }
+
+    }
+}
---
+++ MovieMania/MovieMania/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieMania.Models
+{
+    [Index(nameof(movieName))]
+
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Enter a valid name")]
+        public string movieName { get; set; }
+        [Required(ErrorMessage = "Enter a valid Producer name")]
+        public string producer { get; set; }
+        [Required(ErrorMessage = "Enter a valid ticket price")]
+        public double ticket { get; set; }
+        [Required(ErrorMessage = "Enter a valid release date")]
+        public DateOnly releaseDate { get; set; }
+    }
+}
